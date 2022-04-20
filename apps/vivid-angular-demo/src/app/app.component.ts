bef6f7f09aef1dff443f2a09f19c634f5becbe89
yonatankra <yonatan.kra@vonage.com>
2022-04-20 12:37:35 +0300

    initial commit

---
+++ apps/vivid-angular-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vivid-spring-hack-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'vivid-angular-demo';
+}
